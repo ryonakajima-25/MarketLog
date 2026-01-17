426e7de87728e57fc3f2ec34109cf94352c775bc
中島 良 <nakajimaryo@nakajimaryo.local>
2026-01-17 17:15:49 +0900

    Fix environment and add requirements.txt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nakajimaryo/stock-app/venv"
+set -gx VIRTUAL_ENV "/Users/nakajimaryo/MarketLog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
